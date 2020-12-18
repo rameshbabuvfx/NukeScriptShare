04db5a9c4d415e4dd39bd78b68e94682cbc2c3fb
DELL <rameshkanna95@gmail.com>
2020-12-18 16:41:32 +0530

    loaded Ui dile

--- NukeShareScript.py
+++ NukeShareScript.py
@@ -7,15 +7,21 @@ import sys
 sys.path.append('F:/PYTHON/Python calculator/venv/Lib/site-packages')
 
 
-from PySide2.QtWidgets import *
-from PySide2.QtCore import *
-from PySide2.QtGui import *
-from PySide2.QtUiTools import QUiLoader
+try:
+    from PySide2.QtWidgets import *
+    from PySide2.QtCore import *
+    from PySide2.QtGui import *
+    from PySide2.QtUiTools import QUiLoader
+except:
+    from PySide.QtCore import *
+    from PySide.QtGui import *
+    from PySide.QtUiTools import QUiLoader
+
+
 import nuke
 import getpass,datetime,os
 import pymongo
 from bson import ObjectId
-from mainUI import Ui_MainWindow
 
 
 #####   Connecting Mongodb server    #######
@@ -29,72 +35,79 @@ username = getpass.getuser()
 
 
 
-class MainWindow(QMainWindow,Ui_MainWindow):
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
-        self.setupUi(self)
+        #### load Ui ###
+
+        loader = QUiLoader()
+        self.window = loader.load("ShareScriptUI.ui")
+
+
+
         self.createUser()
         self.userList()
         self.receivedScriptList()
         self.sentRecentList()
         self.FavouritesList()
         self.autoDelete()
-        self.show()
+        self.window.show()
 
-        self.ShareButton.clicked.connect(self.insertData)
+        self.window.ShareButton.clicked.connect(self.insertData)
 
 
         self.completer = QCompleter(self.artistNames)
         self.completer.setCaseSensitivity(Qt.CaseInsensitive)
-        self.SenderName.addItems(self.artistNames)
-        self.SenderName.setEditable(True)
-        self.SenderName.completer().setCompletionMode(QCompleter.PopupCompletion)
-        self.SenderName.setInsertPolicy(QComboBox.NoInsert)
-        self.SenderName.setCompleter(self.completer)
+        self.window.SenderName.addItems(self.artistNames)
+        self.window.SenderName.setEditable(True)
+        self.window.SenderName.completer().setCompletionMode(QCompleter.PopupCompletion)
+        self.window.SenderName.setInsertPolicy(QComboBox.NoInsert)
+        self.window.SenderName.setCompleter(self.completer)
 
 
-        self.ReceivedList.setHorizontalHeaderLabels(['Script-Name', 'Sender', 'Time   '])
-        self.ReceivedList.setSelectionBehavior(QAbstractItemView.SelectRows)
-        self.ReceivedList.hideColumn(3)
-        self.ReceivedList.itemClicked.connect(self.clickedReceivedList)
-        self.ReceivedList.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.ReceivedList.customContextMenuRequested.connect(self.addFavourites)
+        self.window.ReceivedList.setHorizontalHeaderLabels(['Script-Name', 'Sender', 'Time   '])
+        self.window.ReceivedList.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.window.ReceivedList.hideColumn(3)
+        self.window.ReceivedList.itemClicked.connect(self.clickedReceivedList)
+        self.window.ReceivedList.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.window.ReceivedList.customContextMenuRequested.connect(self.addFavourites)
 
 
-        self.SentList.setHorizontalHeaderLabels(['Script-Name', 'Sent-To', 'Time   '])
-        self.SentList.setSelectionBehavior(QAbstractItemView.SelectRows)
-        self.SentList.hideColumn(3)
-        self.SentList.itemClicked.connect(self.clickedSentList)
+        self.window.SentList.setHorizontalHeaderLabels(['Script-Name', 'Sent-To', 'Time   '])
+        self.window.SentList.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.window.SentList.hideColumn(3)
+        self.window.SentList.itemClicked.connect(self.clickedSentList)
 
 
-        self.Fav_Table.setHorizontalHeaderLabels(['Script-Name', 'Time   '])
-        self.Fav_Table.setSelectionBehavior(QAbstractItemView.SelectRows)
-        self.Fav_Table.hideColumn(3)
+        self.window.Fav_Table.setHorizontalHeaderLabels(['Script-Name', 'Time   '])
+        self.window.Fav_Table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.window.Fav_Table.hideColumn(3)
 
 
-        self.Fav_Paste.clicked.connect(self.pasteScript)
+        self.window.Fav_Paste.clicked.connect(self.pasteScript)
 
-        self.Fav_delete.clicked.connect(self.deleteFav)
+        self.window.Fav_delete.clicked.connect(self.deleteFav)
 
-        self.ReceivedNameEdit.setText(username)
+        self.window.ReceivedNameEdit.setText(username)
 
-        self.SentNameEdit.setText(username)
+        self.window.SentNameEdit.setText(username)
 
-        self.Pastebutton.clicked.connect(self.pasteScript)
+        self.window.Pastebutton.clicked.connect(self.pasteScript)
 
-        self.Refreshbutton.clicked.connect(self.refreshApp)
+        self.window.Refreshbutton.clicked.connect(self.refreshApp)
 
 
     def clickedReceivedList(self):
 
-        self.SentList.clearSelection()
+        self.window.SentList.clearSelection()
         self.RL = True
 
 
     def clickedSentList(self):
 
-        self.ReceivedList.clearSelection()
+        self.window.ReceivedList.clearSelection()
         self.RL = False
 
 
@@ -126,7 +139,7 @@ class MainWindow(QMainWindow,Ui_MainWindow):
             ScriptName = 'untitled'
         else :
             pass
-        Sending_To = self.SenderName.currentText()
+        Sending_To = self.window.SenderName.currentText()
         nuke.nodeCopy("%clipboard%")
         script = QApplication.clipboard().text()
 
@@ -140,7 +153,7 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         else:
             pass
 
-############# Inserting in SentList ############
+############# Inserting in window.SentList ############
 
         var = COLLECTION.find_one({'user': username})
         sentlen = len(var['Sent']) - 1
@@ -154,7 +167,7 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         receivedlen = len(var['Received']) - 1
         receivedlen = 1 + receivedlen
 
-        COLLECTION.update_one({'user':Sending_To},{'$set':{'Received.{}'.format(receivedlen):{'SenderName':username,'ScriptName':ScriptName,'script':script,'date':now,'_id':ObjectId()}}})
+        COLLECTION.update_one({'user':Sending_To},{'$set':{'Received.{}'.format(receivedlen):{'window.SenderName':username,'ScriptName':ScriptName,'script':script,'date':now,'_id':ObjectId()}}})
 
         self.refreshApp()
 
@@ -162,8 +175,8 @@ class MainWindow(QMainWindow,Ui_MainWindow):
 
         nukedata = COLLECTION.find_one({'user':username})
         reclen = len(nukedata['Received'])
-        self.ReceivedList.setRowCount(reclen)
-        self.ReceivedList.setColumnCount(4)
+        self.window.ReceivedList.setRowCount(reclen)
+        self.window.ReceivedList.setColumnCount(4)
 
         for x,i in enumerate(nukedata['Received']):
 
@@ -172,18 +185,18 @@ class MainWindow(QMainWindow,Ui_MainWindow):
             time = self.time_difference(i['date'])
             id = str(i['_id'])
 
-            self.ReceivedList.setItem(x,0,QTableWidgetItem(ScriptName))
-            self.ReceivedList.setItem(x,1,QTableWidgetItem(SenderName))
-            self.ReceivedList.setItem(x,2,QTableWidgetItem(time))
-            self.ReceivedList.setItem(x,3,QTableWidgetItem(id))
+            self.window.ReceivedList.setItem(x,0,QTableWidgetItem(ScriptName))
+            self.window.ReceivedList.setItem(x,1,QTableWidgetItem(SenderName))
+            self.window.ReceivedList.setItem(x,2,QTableWidgetItem(time))
+            self.window.ReceivedList.setItem(x,3,QTableWidgetItem(id))
 
 
     def sentRecentList(self):
 
         nukedata = COLLECTION.find_one({'user':username})
         sentlen = len(nukedata['Sent'])
-        self.SentList.setRowCount(sentlen)
-        self.SentList.setColumnCount(4)
+        self.window.SentList.setRowCount(sentlen)
+        self.window.SentList.setColumnCount(4)
 
         for x,i in enumerate(nukedata['Sent']):
 
@@ -192,17 +205,17 @@ class MainWindow(QMainWindow,Ui_MainWindow):
             time = self.time_difference(i['date'])
             id = str(i['_id'])
 
-            self.SentList.setItem(x,0,QTableWidgetItem(ScriptName))
-            self.SentList.setItem(x,1,QTableWidgetItem(Send_To))
-            self.SentList.setItem(x,2,QTableWidgetItem(time))
-            self.SentList.setItem(x,3,QTableWidgetItem(id))
+            self.window.SentList.setItem(x,0,QTableWidgetItem(ScriptName))
+            self.window.SentList.setItem(x,1,QTableWidgetItem(Send_To))
+            self.window.SentList.setItem(x,2,QTableWidgetItem(time))
+            self.window.SentList.setItem(x,3,QTableWidgetItem(id))
 
     def FavouritesList(self):
 
         nukedata = COLLECTION.find_one({'user':username})
         sentlen = len(nukedata['Favs'])
-        self.Fav_Table.setRowCount(sentlen)
-        self.Fav_Table.setColumnCount(4)
+        self.window.Fav_Table.setRowCount(sentlen)
+        self.window.Fav_Table.setColumnCount(4)
 
         for x,i in enumerate(nukedata['Favs']):
 
@@ -210,17 +223,17 @@ class MainWindow(QMainWindow,Ui_MainWindow):
             time = self.time_difference(i['date'])
             id = str(i['_id'])
 
-            self.Fav_Table.setItem(x,0,QTableWidgetItem(ScriptName))
-            self.Fav_Table.setItem(x,2,QTableWidgetItem(time))
-            self.Fav_Table.setItem(x,3,QTableWidgetItem(id))
+            self.window.Fav_Table.setItem(x,0,QTableWidgetItem(ScriptName))
+            self.window.Fav_Table.setItem(x,2,QTableWidgetItem(time))
+            self.window.Fav_Table.setItem(x,3,QTableWidgetItem(id))
 
 
 
     def pasteScript(self):
 
         if self.RL == True:
-            row = self.ReceivedList.currentRow()
-            CurrentId = self.ReceivedList.item(row,3).text()
+            row = self.window.ReceivedList.currentRow()
+            CurrentId = self.window.ReceivedList.item(row,3).text()
             allItems = COLLECTION.find_one({'user':username},)
             var = allItems['Received']
             for i in var:
@@ -228,8 +241,8 @@ class MainWindow(QMainWindow,Ui_MainWindow):
                     item = i['script']
 
         else:
-            row = self.SentList.currentRow()
-            CurrentId = self.SentList.item(row,3).text()
+            row = self.window.SentList.currentRow()
+            CurrentId = self.window.SentList.item(row,3).text()
             allItems = COLLECTION.find_one({'user':username},)
             var = allItems['Sent']
             for i in var:
@@ -259,9 +272,9 @@ class MainWindow(QMainWindow,Ui_MainWindow):
 
         menu = QMenu()
         fav = menu.addAction('add fav')
-        action = menu.exec_(self.ReceivedList.mapToGlobal(pos))
-        selectedRow = self.ReceivedList.currentRow()
-        item = self.ReceivedList.item(selectedRow,3).text()
+        action = menu.exec_(self.window.ReceivedList.mapToGlobal(pos))
+        selectedRow = self.window.ReceivedList.currentRow()
+        item = self.window.ReceivedList.item(selectedRow,3).text()
         recList = COLLECTION.find_one({'user':username})
         recList = recList['Received']
         for i in recList:
@@ -300,8 +313,8 @@ class MainWindow(QMainWindow,Ui_MainWindow):
 
     def deleteFav(self):
 
-        selectedRow = self.Fav_Table.currentRow()
-        item = self.Fav_Table.item(selectedRow,3).text()
+        selectedRow = self.window.Fav_Table.currentRow()
+        item = self.window.Fav_Table.item(selectedRow,3).text()
         collectdoc = COLLECTION.find_one({'user':username})
         collectdoc = collectdoc['Favs']
         for i in collectdoc:
@@ -319,8 +332,8 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.FavouritesList()
 
 
-# if __name__ == '__main__':
-#
-#     app = QApplication(sys.argv)
-#     window = MainWindow()
-#     sys.exit(app.exec_())
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
